eb69776652c947ea859001154391ca54701d6ad6
Gil Alkobi <86377486+gilalk@users.noreply.github.com>
2022-05-02 16:57:46 +0300

    npm install mongodb + Connect to MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const bodyPhraser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
+MongoClient.connect('mongodb://localhost:27017', (err, db) => {
 
-app.get('/', (req,res) => {
+    if (err) {
+        console.log('Can NOT connect to MongoDB');
+        process.exit(1);
+    }
+
+    console.log('Connected to MongoDB');
+
+});
+
+//נוסיף מידלוור
+
+app.use(bodyPhraser.urlencoded({ extended: true }));
+
+//הגדרות
+
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body);
 });
 
 app.listen(3000, () => {
